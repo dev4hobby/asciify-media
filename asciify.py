a6a35d78d127bfe70ec5af39c45996f8d76870dc
Seongchuel, Ahn <aciddust20@gmail.com>
2020-03-26 17:55:29 +0900

    Update, asciify media file (image, video or something...)

---
+++ asciify.py
@@ -0,0 +1,128 @@
+import numpy as np
+import cv2
+import sys
+
+'''  
+ascii level 참조
+http://paulbourke.net/dataformats/asciiart/ 
+'''
+# 70단계의 회색값
+gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+# 10단계의 회색값
+gscale2 = '@%#*+=-:. '
+
+def image2ascii(filename, cols, scale, level): 
+    global gscale1, gscale2
+  
+    # 이미지 열어서 grayscale 형식으로 바꿈. 
+    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    if image:
+        size = [len(image[0]), len(image)]
+    else:
+        print('영상 정보 획득 실패')
+        sys.exit(1)
+
+    print("input image dims: %d x %d" % (size[0], size[1])) 
+  
+    # filter의 width, height 계산
+    w = size[0] / cols 
+    h = w / scale 
+    rows = int(size[1] / h)
+      
+    print("cols: %d, rows: %d" % (cols, rows)) 
+    print("tile dims: %d x %d" % (w, h)) 
+  
+    # 이미지 크기가 너무 작으면 안됨.
+    if cols > size[0] or rows > size[1]:
+        print("너무 작은 이미지")
+        exit(0)
+  
+    ascii_image = ""
+    for row in range(rows):
+        y1 = int(row*h)
+        y2 = int((row+1)*h)
+
+        # 마지막 인덱스 처리
+        if row == rows-1:
+            y2 = size[1]
+        
+        for col in range(cols):
+            x1 = int(col*w)
+            x2 = int((col+1)*w)
+
+            # 마지막 인덱스 처리
+            if col == cols-1:
+                x2 = size[0]
+            
+            crop = image[y1:y2, x1:x2]
+            avg = crop.mean()
+            if level:
+                gsval = gscale1[int((avg*69)/255)]
+            else:
+                gsval = gscale2[int((avg*9)/255)]
+            
+            ascii_image+=gsval
+        ascii_image+='\n'
+    print(ascii_image)
+    return ascii_image
+
+
+def video2ascii(filename, cols, scale, level): 
+    global gscale1, gscale2
+  
+    vidcap = cv2.VideoCapture(filename)
+    success,image = vidcap.read()
+    # 이미지 크기 확인
+    if success:
+        size = [len(image[0]), len(image)]
+    else:
+        print('영상 정보 획득 실패')
+        sys.exit(1)
+
+    # filter의 width, height 계산
+    w = size[0] / cols
+    h = w / scale
+    rows = int(size[1]/h)
+    # cols = 특정 비율 출력을 원한다면 여기 코딩.
+    # 80*24 기준 터미널창에 딱 맞게 출력하기 위해 cols는 계산하지 않음.
+
+    print("cols: %d, rows: %d" % (cols, rows))
+    print("tile dims: %d x %d" % (w, h))
+  
+    # 이미지 크기가 너무 작으면 안됨.
+    if cols > size[0] or rows > size[1]:
+        print("너무 작은 이미지")
+        exit(0)
+
+    while success:
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        ascii_image = ""
+        for row in range(rows):
+            y1 = int(row*h)
+            y2 = int((row+1)*h)
+
+            # 마지막 인덱스 처리
+            if row == rows-1:
+                y2 = size[1]
+            
+            for col in range(cols):
+                x1 = int(col*w)
+                x2 = int((col+1)*w)
+
+                # 마지막 인덱스 처리
+                if col == cols-1:
+                    x2 = size[0]
+                
+                crop = image[y1:y2, x1:x2]
+                avg = crop.mean()
+                if level:
+                    gsval = gscale1[int((avg*69)/255)]
+                else:
+                    gsval = gscale2[int((avg*9)/255)]
+                
+                ascii_image+=gsval
+            ascii_image+='\n'
+        print(ascii_image)
+        success, image = vidcap.read()
+
+    return ascii_image    
---
+++ run.py
@@ -0,0 +1,51 @@
+import argparse
+import asciify
+
+def main():
+    # argv 파싱
+    descStr = "This program converts an image into ASCII art."
+    parser = argparse.ArgumentParser(description=descStr)
+    
+    # 파싱할 것들
+    parser.add_argument('--file', dest='name', required=True)
+    parser.add_argument('--scale', dest='scale', required=False)
+    parser.add_argument('--out', dest='out', required=False)
+    parser.add_argument('--cols', dest='cols', required=False)
+    parser.add_argument('--morelevels',dest='level',action='store_true')
+  
+    # 파싱한거 쓰는부분
+    args = parser.parse_args()
+    name = args.name
+  
+    # set output file
+    out = 'out.txt'
+    if args.out:
+        out = args.out
+  
+    # set scale default as 0.43 which suits
+    # a Courier font
+    scale = 0.43
+    if args.scale:
+        scale = float(args.scale)
+  
+    # set cols
+    cols = 80
+    if args.cols:
+        cols = int(args.cols)
+  
+    print('processing ...')
+    # convert image to ascii txt
+    ext = name.split('.')[-1]
+    if ext in ['jpg','png','jpeg','bmp']:
+        aimg = asciify.image2ascii(name, cols, scale, args.level)
+        f = open(out, 'w')
+        f.write(aimg)
+        print("ASCII art written to %s" % out)
+    elif ext in ['mp4', ]: # 원한다면 다른 확장자 추가 가능.
+        _ = asciify.video2ascii(name, cols, scale, args.level)
+    else:
+        print('Unexpected file type')
+
+# main 시작
+if __name__ == '__main__':
+    main()
